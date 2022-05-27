ef67c2b4e78fe0ce1276c92e6d3a6fdbae32e4e2
Bhavesh Kulluru <38748374+RiskyTrick@users.noreply.github.com>
2022-05-27 12:15:51 +0530

    Delete Java Spring Boot using CRUD and MySQL/Updated directory

--- Java Spring Boot using CRUD and MySQL/Updated/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.CMS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.CMS.entity.Client;
-
-public interface ClientRepository extends JpaRepository<Client,Long> {
-
-	
-}
