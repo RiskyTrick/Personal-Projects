cdb6c61bcc593d86791c8c934c63b0b7ae9518e0
Bhavesh Kulluru <38748374+RiskyTrick@users.noreply.github.com>
2022-07-11 17:32:45 +0530

    Delete AvexaEmployeeData directory

--- AvexaEmployeeData/src/main/java/com/example/AED/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.AED.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.AED.entity.Employee;
-public interface EmployeeRepository extends MongoRepository<Employee,Long>{
-
-	
-}
